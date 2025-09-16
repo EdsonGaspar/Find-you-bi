c5cb920203e944dab3ca4ed456a4ca0ca2a498b6
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2025-09-16 18:52:10 +0100

    feat: Update header and start implement page Post

---
+++ src/app/posters/public.tsx
@@ -0,0 +1,3 @@
+export default function Publics() {
+  return <form>Items a ser cadastrado</form>;
+}
--- src/components/header/search-input.tsx
+++ src/components/header/search-input.tsx
@@ -1,18 +1,21 @@
-"use client";
 import { H1 } from "../H1/h1";
 
 export function SearchInputDesktop() {
   return (
-    <div className="hidden mx-auto py-4 md:flex justify-between items-center">
-      <H1>Desktop</H1>
-    </div>
+    <nav className="hidden mx-auto py-4 md:flex justify-center items-center">
+      <ul className="flex gap-4 border flex-1 justify-around max-w-3xl items-center text-xl rounded-full p-2">
+        <li className="rounded-full bg-accent px-3.5 py-1.5">Início</li>
+        <li className="rounded-full  px-3.5 py-1.5">Procurar</li>
+        <li className="rounded-full  px-3.5 py-1.5 text-">Publicar</li>
+      </ul>
+    </nav>
   );
 }
 
 export function SearchInputMobile() {
   return (
-    <div className="flex mx-auto  py-4 md:hidden justify-between items-center">
+    <nav className="flex mx-auto  py-4 md:hidden justify-between items-center">
       <H1>Mobile</H1>
-    </div>
+    </nav>
   );
 }
